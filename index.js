256fd40ec3e4f2f36cc201f20e7fc0ffd9d47790
MarkiianPerliak <122627880+MarkiianPerliak@users.noreply.github.com>
2024-10-02 15:01:38 +0300

    done

--- index.js
+++ index.js
@@ -1,49 +1,49 @@
 // // start 1.
-// const firstPrompt = prompt("Type something");
+const firstPrompt = prompt("Type something");
 
-// const secondPrompt = prompt("Type something");
+const secondPrompt = prompt("Type something");
 
-// if(firstPrompt && secondPrompt === !"") {
-//     alert("Обидва поля заповнені")
-// } else {
-//     alert("Не всі поля заповнені")
-// }
+if(firstPrompt && secondPrompt === !"") {
+    alert("Обидва поля заповнені")
+} else {
+    alert("Не всі поля заповнені")
+}
 // // end 1.
 
 // // start 2.
-// const numberPromptString = prompt("Напишіть число");
+const numberPromptString = prompt("Напишіть число");
 
-// const numberPrompt = Number(numberPromptString);
+const numberPrompt = Number(numberPromptString);
 
-// if(numberPrompt > 10) {
-//     alert("Сума більша за 10")
-// } else {
-//     alert("Сума менша або дорівнює 10")
-// }
+if(numberPrompt > 10) {
+    alert("Сума більша за 10")
+} else {
+    alert("Сума менша або дорівнює 10")
+}
 // // end 2.
 
 // // start 3.
-// const string = prompt("Type something");
+const string = prompt("Type something");
 
-// const stringPrompt = string.includes("JavaScript");
+const stringPrompt = string.includes("JavaScript");
 
-// if(stringPrompt === true) {
-//     alert("Текст містить слово JavaScript")
-// } else {
-//     alert("Текст не містить слово JavaScript")
-// }
+if(stringPrompt === true) {
+    alert("Текст містить слово JavaScript")
+} else {
+    alert("Текст не містить слово JavaScript")
+}
 // // end 3.
 
 // // start 4.
-// const numberString = prompt("Напишіть число");
+const numberString = prompt("Напишіть число");
 
-// const numberPrompt1 = Number(numberString);
+const numberPrompt1 = Number(numberString);
 
-// if(numberPrompt1 > 10 && numberPrompt1 < 20) {
-//     alert("Число входить в діапазон від 10 до 20")
-// } else {
-//     alert("Число не входить в діапазон від 10 до 20")
-// }
+if(numberPrompt1 > 10 && numberPrompt1 < 20) {
+    alert("Число входить в діапазон від 10 до 20")
+} else {
+    alert("Число не входить в діапазон від 10 до 20")
+}
 // end 4.
 
 // start 5.
@@ -53,7 +53,7 @@ const formEmail = prompt("Email");
 
 const formPassword = prompt("Password");
 
-if(formEmail.includes("@", ".") && formName.indexOf(2) && formPassword.indexOf(5) === true) {
+if(formName.length >= 3 && formEmail.includes("@") && formEmail.includes(".") && formPassword.length >= 6) {
     alert("Confirmed")
 } else {
     alert("Error")
